5c60bbdec0c1993ffba6330e56ea85ce76a00a23
MANISH KUMAR <109017879+manishkr2111@users.noreply.github.com>
2023-06-05 08:12:28 +0530

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-const endDate = "30 may 2023 12:00:00 PM"
+const endDate = "30 may 2024 12:00:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
